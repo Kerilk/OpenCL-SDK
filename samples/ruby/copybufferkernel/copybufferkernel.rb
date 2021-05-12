dffc03e40696af8d682f9b1903da59c55ea9a0ee
Brice Videau <bvideau@anl.gov>
2021-05-12 16:10:12 -0500

    Add more sample and slight refactoring.

--- samples/ruby/copybufferkernel/copybufferkernel.rb
+++ samples/ruby/copybufferkernel/copybufferkernel.rb
@@ -8,6 +8,15 @@ OptionParser.new do |parser|
 end.parse!(into: options)
 
 require 'opencl_ruby_ffi'
+require 'narray_ffi'
+
+kernel_string = <<EOF
+kernel void CopyBuffer( global uint* dst, global uint* src )
+{
+    uint id = get_global_id(0);
+    dst[id] = src[id];
+}
+EOF
 
 buffer_size = 1024 * 1024
 
@@ -21,19 +30,23 @@ puts "Running on device: #{d.name}"
 c = OpenCL.create_context(d)
 q = c.create_command_queue(d)
 
+program = c.create_program_with_source(kernel_string)
+program.build
+
 device_src = c.create_buffer(buffer_size*OpenCL::UInt.size, flags: OpenCL::Mem::ALLOC_HOST_PTR)
 device_dst = c.create_buffer(buffer_size*OpenCL::UInt.size, flags: OpenCL::Mem::ALLOC_HOST_PTR)
 
 _, host_ptr = q.enqueue_map_buffer(device_src, OpenCL::MapFlags::WRITE_INVALIDATE_REGION, blocking: true)
-host_ptr.write_array_of_uint(buffer_size.times.to_a)
+arr = NArray.to_na(host_ptr, NArray::INT)
+buffer_size.times { |i| arr[i] = i }
 q.enqueue_unmap_mem_object(device_src, host_ptr)
 
-q.enqueue_copy_buffer(device_src, device_dst)
+program.CopyBuffer(q, [buffer_size], device_dst, device_src)
 
 _, host_ptr = q.enqueue_map_buffer(device_dst, OpenCL::MapFlags::READ, blocking: true)
-result = host_ptr.read_array_of_uint(buffer_size)
+arr = NArray.to_na(host_ptr, NArray::INT)
 buffer_size.times { |i|
-  raise "invalid copy: wanted #{i}, got #{result[i]}" unless result[i] == i
+  raise "invalid copy: wanted #{i}, got #{arr[i]}" unless arr[i] == i
 }
 q.enqueue_unmap_mem_object(device_dst, host_ptr)
 

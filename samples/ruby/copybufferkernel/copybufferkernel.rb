f0f095e46a547949986a22f0f9ed70510988af31
Brice Videau <bvideau@anl.gov>
2021-05-13 16:23:01 -0500

    Use test matrix and add syntax check.

--- samples/ruby/copybufferkernel/copybufferkernel.rb
+++ samples/ruby/copybufferkernel/copybufferkernel.rb
@@ -1,29 +1,33 @@
+# frozen_string_literal: true
+
 require 'optparse'
 
 options = { platform: 0, device: 0 }
 OptionParser.new do |parser|
   parser.banner = "Usage: ruby #{File.basename($PROGRAM_NAME)} [options]"
-  parser.on("-p", "--platform INDEX", Integer, "Index of the platform to use (default 0)")
-  parser.on("-d", "--device INDEX", Integer, "Index of the device to use (default 0)")
+  parser.on('-p', '--platform INDEX', Integer, 'Index of the platform to use (default 0)')
+  parser.on('-d', '--device INDEX', Integer, 'Index of the device to use (default 0)')
 end.parse!(into: options)
 
 require 'opencl_ruby_ffi'
 require 'narray_ffi'
 
-kernel_string = <<EOF
-kernel void CopyBuffer( global uint* dst, global uint* src )
-{
-    uint id = get_global_id(0);
-    dst[id] = src[id];
-}
-EOF
+kernel_string = <<~CLC
+  kernel void CopyBuffer( global uint* dst, global uint* src )
+  {
+      uint id = get_global_id(0);
+      dst[id] = src[id];
+  }
+CLC
 
 buffer_size = 1024 * 1024
 
 p = OpenCL.platforms[options[:platform]]
 raise "Invalid platform index #{options[:platform]}" unless p
+
 d = p.devices[options[:device]]
 raise "Invalid device index #{options[:device]}" unless d
+
 puts "Running on platform: #{p.name}"
 puts "Running on device: #{d.name}"
 
@@ -33,8 +37,8 @@ begin
 
   program = c.create_program_with_source(kernel_string)
   program.build
-  device_src = c.create_buffer(buffer_size*OpenCL::UInt.size, flags: OpenCL::Mem::ALLOC_HOST_PTR)
-  device_dst = c.create_buffer(buffer_size*OpenCL::UInt.size, flags: OpenCL::Mem::ALLOC_HOST_PTR)
+  device_src = c.create_buffer(buffer_size * OpenCL::UInt.size, flags: OpenCL::Mem::ALLOC_HOST_PTR)
+  device_dst = c.create_buffer(buffer_size * OpenCL::UInt.size, flags: OpenCL::Mem::ALLOC_HOST_PTR)
 
   _, host_ptr = q.enqueue_map_buffer(device_src, OpenCL::MapFlags::WRITE_INVALIDATE_REGION, blocking: true)
   arr = NArray.to_na(host_ptr, NArray::INT)
@@ -45,19 +49,17 @@ begin
 
   _, host_ptr = q.enqueue_map_buffer(device_dst, OpenCL::MapFlags::READ, blocking: true)
   arr = NArray.to_na(host_ptr, NArray::INT)
-  buffer_size.times { |i|
+  buffer_size.times do |i|
     raise "invalid copy: wanted #{i}, got #{arr[i]}" unless arr[i] == i
-  }
+  end
   q.enqueue_unmap_mem_object(device_dst, host_ptr)
 
-  puts "Success."
+  puts 'Success.'
 rescue OpenCL::Error::BUILD_PROGRAM_FAILURE
-  puts "Compilation of program failed:"
-  program.build_log.each { |device, log|
+  puts 'Compilation of program failed:'
+  program.build_log.each do |device, log|
     puts " - #{device.name}:"
     puts log
-  }
-  puts "Expected Failure on MacOS..."
+  end
+  puts 'Expected Failure on MacOS...'
 end
-
-
